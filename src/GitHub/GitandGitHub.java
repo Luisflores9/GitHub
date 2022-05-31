b0e13f56aa959d29a4c3e85bdf8cd9abba2ba923
Luisflores9 <Luis.fsanchez09@gmail.com>
2022-05-31 19:36:47 -0400

    1st commit

---
+++ src/GitHub/GitandGitHub.java
@@ -0,0 +1,11 @@
+package GitHub;
+
+public class GitandGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Today we learn GitHub");
+		
+	}
+
+}
