671d62c453197da6cef0b2283499184f8ade4e60
Luisflores9 <Luis.fsanchez09@gmail.com>
2022-05-31 19:54:59 -0400

    2nd commit

--- src/GitHub/GitandGitHub.java
+++ src/GitHub/GitandGitHub.java
@@ -5,7 +5,9 @@ public class GitandGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Today we learn GitHub");
+		System.out.println("Today was a good day");
 		
+//		
 	}
 
 }
